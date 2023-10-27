5582ad8cddc95887c9ee8d1227fddac05d5ed48c
DhinakaranS-75 <dhinakaran75493@gmail.com>
2023-10-27 17:36:01 +0530

    update codes

---
+++ front-end/src/App.js
@@ -0,0 +1,13 @@
+import PhoneAndroidIcon from '@mui/material/PhoneAndroid';
+
+function App() {
+  return (
+    <div>
+      this my App Page
+      <PhoneAndroidIcon />
+    </div>
+
+  );
+}
+
+export default App;
