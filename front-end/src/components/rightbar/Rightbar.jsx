fb729bed8a4a11fc432b037bae97e0c5fd4bbda3
DhinakaranS-75 <dhinakaran75493@gmail.com>
2023-10-29 18:22:34 +0530

    rightbar

--- front-end/src/components/rightbar/Rightbar.jsx
+++ front-end/src/components/rightbar/Rightbar.jsx
@@ -2,5 +2,18 @@ import React from "react";
 import "./rightbar.css";
 
 export default function Rightbar() {
-  return <div className="rightbar">right bar</div>;
+  return (
+    <div className="rightbar">
+      <div className="rightWarapper">
+        <div className="birthdayContainer">
+          <img className="birthdayImg" src="assets/gift.png" alt="" />
+          <span className="birthdayText">
+            <b>Pola Foster</b> and 3 <b> Other friends</b> Hav a Birthday today
+          </span>
+        </div>
+        <img className="rightbarAd" src="assets/ad.png" alt="" />
+        <h4 className="rightbarTitle">Online Friends</h4>
+      </div>
+    </div>
+  );
 }
